5097f0e57deb18ca4b2c5ca306a6a29dcbd9eeec
The Ultimate Winner <harshadchawra@gmail.com>
2018-01-03 21:54:04 +0530

    vid 3 first http call

--- dist/app.component.js
+++ dist/app.component.js
@@ -5,20 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(http) {
+        this.http = http;
         this.message = 'This is the sample message.';
     }
+    AppComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        //grab users
+        this.http.get('https://reqres.in/api/users')
+            .subscribe(function (data) {
+            console.log(data.json());
+            _this.users = data.json().data;
+        });
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        styles: ["\n    \n  "],
-        template: "\n    <div class=\"jumbotron text-center\">\n      <h1>The App Lives!</h1>\n      <p>{{ message }}</p>\n    </div>\n  "
-    })
+        template: "\n    <div class=\"jumbotron text-center\">\n      <h1>The App Lives!</h1>\n      <p>{{ message }}</p>\n    </div>\n\n    <div *ngIf=\"users\">\n      <div *ngFor=\"let user of users\">\n        <h2>{{user.first_name}}</h2>\n      </div>\n    </div>  \n  "
+    }),
+    __metadata("design:paramtypes", [http_1.Http])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
